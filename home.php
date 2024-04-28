760aa24692458f8f36185dbb4f9010090ffbc983
Kyora02 <kedrick.adyatma@gmail.com>
2024-04-28 22:37:41 +0700

    home progress

--- home.php
+++ home.php
@@ -28,14 +28,15 @@
         </li>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
+            Category
           </a>
           <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
+            <li><a class="dropdown-item" href="#">Mobile</a></li>
+            <li><a class="dropdown-item" href="#">PC Game</a></li>
+            <li><a class="dropdown-item" href="#">Voucher</a></li>
+            <li><a class="dropdown-item" href="#">Streaming</a></li>
+            <li><a class="dropdown-item" href="#">Console</a></li>
+        </ul>
         </li>
       </ul>
       <form class="d-flex" role="search">
@@ -46,4 +47,5 @@
   </div>
 </nav>
 </body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </html>
