832cde25a89f592ae9d8489cabe59af643b078bc
Kyora02 <kedrick.adyatma@gmail.com>
2024-04-28 19:23:57 +0700

    login form

--- login.php
+++ login.php
@@ -5,8 +5,22 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+<style>
+    .box{
+        background-color: #F1EEDC;
+    }
+</style>
 <body>
-    <h1>Hello World</h1>
-    <h1>LOGIN</h1>
+    <div class="container mt-5 w-50">
+        <h1 class="text-center">Login</h1>
+        <div class="row justify-content-center">
+            <div class="col-sm-6">
+                <div class="box border rounded p-5">
+
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
