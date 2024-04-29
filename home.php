23ea67d7f74020b0f08307d6618d6eaba9a4a738
ricoricko <ricofrede20@gmail.com>
2024-04-29 18:08:27 +0700

    HELLO WORD

--- home.php
+++ home.php
@@ -47,6 +47,7 @@
   </div>
 </nav>
     <h1>HELLO WORLD</h1>
+    <h1>HELLO TEST COMMIT RICO</h1>
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </html>
