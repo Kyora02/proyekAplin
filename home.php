2b00a8a819ceb6f64e6fe379599537b379b15499
ricoricko <ricofrede20@gmail.com>
2024-04-29 20:33:10 +0700

    ici

--- home.php
+++ home.php
@@ -70,7 +70,7 @@
             <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
         </form>
     </div>
-<h1>tes</h1>
+<h2>asdasdasdasdasd</h2>
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </html>
