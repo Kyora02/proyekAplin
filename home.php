6cc04c34041b9052846525e9919e0e073ad92258
Willwilliamt <163222918+Willwilliamt@users.noreply.github.com>
2024-04-29 21:56:19 +0700

    baru

--- home.php
+++ home.php
@@ -97,7 +97,9 @@
       Streaming
     </div>
 </div>
-
+<div id="belicepat">
+  <img src="Assets/thunder.jpg" alt=""> Beli Cepat
+</div>
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </html>
