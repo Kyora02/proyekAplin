4c82b172e733bd3f6b6029a2dde5bbe162dd87d0
Willwilliamt <163222918+Willwilliamt@users.noreply.github.com>
2024-04-29 21:07:30 +0700

    tes

--- home.php
+++ home.php
@@ -75,6 +75,7 @@
     </div>
   </nav>
   <h2>Test  Ini Rico</h2>
+  <h2>tes</h2>
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </html>
