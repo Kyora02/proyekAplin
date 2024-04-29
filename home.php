e3e7098e51708fcef67ea8790e340ca612600af2
Willwilliamt <163222918+Willwilliamt@users.noreply.github.com>
2024-04-29 21:26:18 +0700

    upload foto

--- home.php
+++ home.php
@@ -75,8 +75,11 @@
     </div>
   </nav>
   </div>
-  <div id="listgame">
-    free fire
+  <div id="listgame" style="display:flex;">
+    <div id="isi">
+      top up game
+
+    </div>
   </div>
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
