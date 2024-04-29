888d57362230a5a874a56da935eb8cca65467de9
ricoricko <ricofrede20@gmail.com>
2024-04-29 20:43:24 +0700

    change 1

--- home.php
+++ home.php
@@ -92,6 +92,12 @@
     <h2>RICO</h2>
     <h2>RICO</h2>
     <h2>RICO</h2>
+
+    <h3>asdasdasdasdasd</h3>
+    <h3>asdasdasdasdasd</h3>
+    <h3>asdasdasdasdasd</h3>
+    <h3>asdasdasdasdasd</h3>
+    <h3>asdasdasdasdasd</h3>
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </html>
