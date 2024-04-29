99a63970488cee7ae37fd63354567ac94bb4c1a9
Willwilliamt <163222918+Willwilliamt@users.noreply.github.com>
2024-04-29 20:06:24 +0700

    rico hamil

--- home.php
+++ home.php
@@ -47,6 +47,18 @@
   </div>
 </nav>
     <h1>HELLO WORLD</h1>
+    <table>
+      <tr>
+        <td>nama</td>
+        <td>password</td>
+      </tr>
+      <tr>
+        <td>
+          rico
+        </td>
+        <td>akuhamil</td>
+      </tr>
+    </table>
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </html>
