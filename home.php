dee4ee3f22c61932047b4496cad3c98765604a20
ricoricko <ricofrede20@gmail.com>
2024-04-29 21:09:47 +0700

    tes

--- home.php
+++ home.php
@@ -47,5 +47,7 @@
   </div>
 </nav>
 </body>
+<h2>POEWEr</h2>
+<h2>ASD</h2>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </html>
